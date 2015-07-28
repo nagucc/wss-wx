75754b50bfb9f00a47da14b5f5d39da0fa4a02f0
Ynu AppEngine <yae@ynu.edu.cn>
2015-07-28 16:56:17 +0800

    test

--- app/controllers/task-notice.js
+++ app/controllers/task-notice.js
@@ -83,9 +83,9 @@ var EventHandlers = {
             // res.reply(config.wss_db);
             var conn = mysql.createConnection(config.wss_db);
             conn.connect();
-            conn.query('SELECT * FROM tk_task where tid > 10', function (err, rows, fields) {
+            conn.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
             	if(err) res.reply(JSON.stringify(err));
-                else res.reply(JSON.stringify(rows || 'nul'));
+                else res.reply('The solution is: ' + rows[0].solution);
             });
             conn.end();
         });
